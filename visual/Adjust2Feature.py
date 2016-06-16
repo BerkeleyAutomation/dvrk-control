fb1bf35335cadd473b51a3c4d60914a0354187e0
yjen <yjen@berkeley.edu>
2016-06-15 21:12:18 -0700

    Updated

--- visual/Adjust2Feature.py
+++ visual/Adjust2Feature.py
@@ -4,7 +4,9 @@ from visualization_msgs.msg import Marker
 import cv2
 import cv_bridge
 import numpy as np
+import scipy
 import scipy.misc
+
 import pickle
 from matplotlib import pyplot as plt
 
@@ -26,11 +28,10 @@ class Adjust2Feature:
         self.bridge = cv_bridge.CvBridge()
         self.block = False
 
+        self.LOWERB = np.array([0,0,0])
+        self.UPPERB = np.array([70,70,70])
 
-        #self.camera_info = self.load_camera_info()
         self.camera_matrix = self.load_camera_matrix()
-        #self.camera_model = image_geometry.PinholeCameraModel()
-        #self.camera_model.fromCameraInfo(self.camera_info)
 
         rospy.init_node('image_saver', anonymous=True)
 
@@ -42,29 +43,19 @@ class Adjust2Feature:
                          self.camera_info_callback, queue_size=1)
 
         rospy.spin()
-        #self.tfl = tf.TransformListener()
 
-        #========SUBSCRIBERS========#
-        # image subscribers
 
     def left_image_callback(self, msg):
         if rospy.is_shutdown() or self.block:
             return
 
         self.left_image = self.bridge.imgmsg_to_cv2(msg, "rgb8")
-        #plt.imshow(self.left_image)
-        #plt.show()
 
     def position_callback(self, msg):
         if rospy.is_shutdown() or self.block:
             return
 
         self.robot_pose = tfx.pose(msg) 
-        
-
-        #print self.robot_pose 
-        #plt.imshow(self.left_image)
-        #plt.show()
 
     def camera_info_callback(self, msg):
         if rospy.is_shutdown() or self.block:
@@ -78,78 +69,90 @@ class Adjust2Feature:
         f.close()
         return info
 
-    def query_point(self):
-        #ps = self.robot_pose.msg.PoseStamped()
-        #print ps
-        #self.tfl.waitForTransform('/endoscope_frame','/world', rospy.Time.now(), rospy.Duration(0.5))
-        #print a
-        #point = self.tfl.transformPoint(self.camera_model.tf_frame, ps)
+    def get_pixel_from3D(self, position=None):
         Trobot = np.zeros((4,4))
         Trobot[:3,:] = np.copy(self.camera_matrix)
         Trobot[3,3] = 1
         Rrobot = np.linalg.inv(Trobot)
 
-        print self.camera_info        
-        #print 'Rrobot', Rrobot, self.camera_info.K
-
         x = np.ones((4,1))
-        x[:3,0] = np.squeeze(self.robot_pose.position)
 
-        #print 'x', x
+        if position == None:
+            x[:3,0] = np.squeeze(self.robot_pose.position)
+        else:
+            x[:3,0] = np.squeeze(position)
 
         cam_frame = np.dot(Rrobot,x)
 
-        #print 'cam_frame', cam_frame
-
         Pcam = np.array(self.camera_info.P).reshape(3,4)
 
-        #print 'p_cam', Pcam
-                
         V = np.dot(Pcam, cam_frame)
-        #print V
 
-        #print np.shape(np.array(self.camera_info.D))
-        #Dcam = np.array(self.camera_info.D).reshape(2,2)
         V = np.array((int(V[0]/V[2]), int(V[1]/V[2])))
 
-        #print "res", V
         return V
 
-    def loopChessBoardPoints(self):
-        pts = pickle.load(open('/home/davinci2/catkin_ws/src/endoscope_calibration/calibration_data/endoscope_points.p', 'rb'))
-        for p in pts:
-            Rrobot = np.zeros((4,4))
-            Rrobot[:3,:] = np.copy(self.camera_matrix)
-            Rrobot[3,3] = 1
+    def get_line_mask(self, position=None, tol=20):
+        hsv = cv2.cvtColor(self.left_image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, self.LOWERB, self.UPPERB)
+        self.left_mask = mask
+        pix = self.get_pixel_from3D(position)
 
-            cmat = np.copy(self.camera_matrix)
-            x = np.ones((4,1))
-            x[:3,0] = np.squeeze(p)
+        if pix[0] < tol or pix[1] < tol:
+            return False
+        elif pix[0] > 1920-tol or pix[1] > 1080-tol:
+            return False
+        else:
+            return (np.sum(mask[pix[1]-tol:pix[1]+tol, pix[0]-tol:pix[0]+ tol]) > 0)
 
-            xr = np.ones((4,1))
-            xr[:3,0] = np.squeeze(np.dot(cmat, x))
+    def get_line_distance(self, position=None, tol=50):
+        hsv = cv2.cvtColor(self.left_image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, self.LOWERB, self.UPPERB)
+        self.left_mask = mask
+        pix = self.get_pixel_from3D(position)
 
-            print p, xr, np.dot(np.linalg.inv(Rrobot), xr)
+        if pix[0] < tol or pix[1] < tol:
+            return np.inf
+        elif pix[0] > 1920-tol or pix[1] > 1080-tol:
+            return np.inf
+        else: 
+            return self.closest_in_window(pix, mask, tol)[1]
+
+    def closest_in_window(self, pix, mask, tol):
+        closest=None
+        dist = lambda u,v: np.abs(u[0]-v[0]) + np.abs(u[1]-v[1])
+
+        for x in range(pix[1]-tol, pix[1]+tol):
+            for y in range(pix[0]-tol, pix[0]+tol):
+                if mask[x,y] != 0 and (closest == None or dist((y,x), pix) < dist(closest, pix)):
+                    closest = (y,x)
+        
+        if closest == None:
+            return None, np.inf
+        else:
+            print closest
+            return closest, dist(closest, pix)
 
 
-
-
-
-
-
+"""
 if __name__ == "__main__":
 
 
     a = Adjust2Feature()
-    pt = a.query_point()
-
-    a.loopChessBoardPoints()
-
+    pt = a.get_pixel_from3D()
 
     img = np.copy(a.left_image)
-    print pt
+    print pt, a.get_line_distance()
     img[pt[1]-10:pt[1]+10,pt[0]-10:pt[0]+10, :] = [0,0,0]
 
+    plt.figure()
+    plt.imshow(a.left_mask)
+    plt.show()
+
+    plt.figure()
     plt.imshow(img)
     plt.show()
+
+    plt.figure()
+"""
 
